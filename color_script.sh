7b837d7302168f89dba9b8b85ec71f5a367d7a3f
ausafumarkhan <ausafumarkhan@gmail.com>
2022-05-17 21:49:50 +0530

    Script to print messages in different color

---
+++ color_script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "This is colourful script"
+
+# This prints in Red Color
+echo -e "\033[0;31m Fail message here"
+
+# This prints in Green color
+echo -e "\033[0;32m Success message here"
+
+# This prints in Yellow color
+echo -e "\033[0;33m Warning message here"
